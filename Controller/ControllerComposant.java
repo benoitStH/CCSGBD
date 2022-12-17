36da57c82dde7f84e8cb9ea6546f265e9d2737b3
benoitStH <95411545+benoitStH@users.noreply.github.com>
2022-12-17 16:15:47 +0100

    Premier dépôt

---
+++ Controller/ControllerComposant.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import java.util.List;
+
+import Dao.ComposantDAO;
+import model.Composant;
+
+public class ControllerComposant {
+
+	private ComposantDAO dao = new ComposantDAO();
+	
+	public List<Composant> GetAllComposants()
+	{
+		return dao.getComposants();
+	}
+	
+	public Composant GetComposantById(int value)
+	{
+		Composant result = dao.getElementById(value);
+		
+		if(result == null)
+		{
+			System.out.println("Erreur : L'id "+value+" ne correspond à aucun composant");
+			return new Composant(-1, "####", null);
+		}
+		
+		return result;
+	}
+}
---
+++ Controller/ControllerMagasin.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import java.util.List;
+
+import Dao.MagasinDAO;
+import model.Magasin;
+import model.Materiaux;
+
+public class ControllerMagasin {
+	
+	private MagasinDAO dao = new MagasinDAO();
+	
+	public List<Magasin> GetAllMagasin()
+	{
+		return dao.getMagasins();
+	}
+	
+	public Magasin getMagasinById(int value)
+	{
+		Magasin result = dao.getElementById(value);
+		
+		if(result == null)
+		{
+			System.out.println("Erreur : L'id "+value+" ne correspond à aucun magasin");
+			return new Magasin(-1, "####", null);
+		}
+		
+		return result;
+	}
+
+}
