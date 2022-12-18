a315b0fba0232eb50c18d51dbb8e478ee5f08b65
benoitStH <95411545+benoitStH@users.noreply.github.com>
2022-12-18 13:02:14 +0100

    Update ControllerCommande.java

--- Controller/ControllerCommande.java
+++ Controller/ControllerCommande.java
@@ -1,5 +1,25 @@
 package Controller;
 
+import model.Commande;
+
 public class ControllerCommande {
 
+    private CommandeDAO dao = new CommandeDAO();
+  
+    public List<Commande> getAllCommandes()
+    {
+          return dao.getCommandes(); 
+    }
+  
+    public Commande getCommandeById(int value)
+    {
+          Commande result = dao.getElementById(value);
+      
+          if(result == null)
+          {
+                System.out.println("Erreur : Aucune commande ne possède l'id "+value); 
+          }
+    }
+  
+  
 }
