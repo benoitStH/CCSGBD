afe63965fe94a19f766958c2dc5a245f657e8d1f
benoitStH <95411545+benoitStH@users.noreply.github.com>
2022-12-18 12:56:40 +0100

    Update ControllerClient.java

--- Controller/ControllerClient.java
+++ Controller/ControllerClient.java
@@ -2,4 +2,25 @@ package Controller;
 
 public class ControllerClient {
 
+    private ClientDAO dao = new ClientDAO();
+  
+    public List<Client> getAllClients
+    {
+          return dao.getClients(); 
+    }
+  
+    public Client getClientById(int value)
+    {
+          Client result = dao.getElementById(value);
+      
+          if(result == null)
+          {
+              System.out.println("Erreur : Aucun client ne possède l'id "+value);
+              return new Client(-1, "###", "###", null, null);
+          }
+      
+          return result;
+    }
+  
+  
 }
