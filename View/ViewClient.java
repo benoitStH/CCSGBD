d30ec1b014dc6bcc5cb135f194daa5d2fb319c5a
benoitStH <95411545+benoitStH@users.noreply.github.com>
2022-12-18 17:35:43 +0100

    Update ViewClient.java

--- View/ViewClient.java
+++ View/ViewClient.java
@@ -60,4 +60,72 @@ public class ViewClient
     }
 
 
+    public Client AddClientTo(Magasin magasin)
+    {
+        Client client = null;
+        String nom, prenom;
+        ControllerCategorie CtrlCat = new ControllerCategorie();
+        List<Categorie> categories = CtrlCat.getAllCategories();
+        List<Integer> seuilMax = new ArrayList<Integer>();
+        
+        System.out.print("Nom du client : ");
+        nom = scan.nextString();
+          
+        System.out.print("Prénom du client : ");
+        prenom = scan.nextString();
+        
+        client = new Client(IdMaxFrom(Ctrl.getAllClients()), nom, prenom, null, null);
+        
+        if(FincClientInMagasin(client, magasin))
+        {
+             System.out.print("Il existe déjà un client avec le même nom et prénom")
+             return null;     
+        }
+        
+        for(int i = 0; i < taille; i++)
+        {
+            System.out.println("Seuil maximal pour la catégorie "+categories.get(i).getNom()+ ": ");
+            seuilMax.add(scan.nextInt());
+        }
+          
+        client = new Client(client.getId(), nom, prenom, categories, seuilMax);
+        
+        Ctrl.AddClientToMagasin(client, magasin);
+          
+        return client;
+          
+    }
+      
+    private boolean FindClientInMagasin(Client client, Magasin magasin)
+    {
+         List<Client> clients = Ctrl.getClientsOf(magasin);
+         int taille = clients.size();
+         boolean trouve = false;
+          
+         for(int i = 0; i < taille && trouve == false; i++)
+         {
+              if(client.getNom() == clients.get(i).getNom() && client.getPrenom() == clients.get(i).getPrenom())
+              {
+                   trouve = true;     
+              }
+         }
+          
+         return trouve;
+         
+    }
+      
+    private int IdMaxFrom(List<Client> clients)
+    {
+            int taille = clients.size();
+            int id = 0;
+          
+            for(int i = 0; i < taille; i++)
+            {
+                 if(id < clients.get(i).getId())
+                 {
+                       id = clients.get(i).getId();
+                 }
+            }
+    }
+      
 }
